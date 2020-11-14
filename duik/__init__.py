6ae0a8620198b964488bf7b295bf27189054a227
Duduf <git@duduf.com>
2020-11-14 14:42:44 +0100

    grouped features to experimental sections

--- duik/__init__.py
+++ duik/__init__.py
@@ -18,7 +18,7 @@
 # <pep8 compliant>
 
 bl_info = {
-    "name": "Duik",
+    "name": "Rx Experimental Rigging & Anim Tools",
     "category": "Rigging",
     "blender": (2, 80, 0),
     "author": "Nicolas 'Duduf' Dufresne",
@@ -42,10 +42,38 @@ from . import (
     oca,
 )
 
+def update_experimental_2d(self, context):
+    preferences = context.preferences.addons[__name__].preferences
+    if preferences.use_experimental_2d:
+        register_experimental_2d()
+    else:
+        unregister_experimental_2d()
+
+def update_experimental_rig(self, context):
+    preferences = context.preferences.addons[__name__].preferences
+    if preferences.use_experimental_rig:
+        register_experimental_rig()
+    else:
+        unregister_experimental_rig()
+
 class DUIK_Preferences( bpy.types.AddonPreferences ):
     # this must match the add-on name, use '__package__'
     # when defining this in a submodule of a python package.
     bl_idname = __name__
+
+    use_experimental_rig: bpy.props.BoolProperty(
+        description="Enable experimental rigging features.",
+        name="Use experimental rigging features",
+        default=False,
+        update=update_experimental_rig
+        )
+
+    use_experimental_2d: bpy.props.BoolProperty(
+        description="Enable experimental 2D features like TexAnim, 2D scenes and OCA import.",
+        name="Use experimental 2D features",
+        default=False,
+        update=update_experimental_2d
+        )
 
     layer_controllers: bpy.props.IntProperty(
         name="Layer for All controllers",
@@ -77,16 +105,21 @@ class DUIK_Preferences( bpy.types.AddonPreferences ):
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Layers")
-        layout.prop(self, "layer_controllers")
-        layout.prop(self, "layer_skin")
-        layout.prop(self, "layer_rig")
-        layout.label(text="Pie Menus")
-        row = layout.row()
-        row.prop(self, "pie_menu_autorig")
-        row.prop(self, "pie_menu_armature_display")
-        row = layout.row()
-        row.prop(self, "pie_menu_animation")
+        
+        layout.prop(self, 'use_experimental_2d')
+        
+        layout.prop(self, 'use_experimental_rig')
+        if self.use_experimental_rig:
+            box = layout.box()
+            box.label(text="Layers:")
+            row = box.row()
+            row.prop(self, "layer_controllers", text="Controllers")
+            row.prop(self, "layer_skin", text="Influences")
+            row.prop(self, "layer_rig", text="Other bones")
+            box.label(text="Pie menus:")
+            box.prop(self, "pie_menu_autorig")
+            box.prop(self, "pie_menu_armature_display")
+            box.prop(self, "pie_menu_animation")           
 
 class DUIK_PT_armature_options( bpy.types.Panel ):
     bl_label = "Duik Layers UI"
@@ -115,33 +148,47 @@ classes = (
     DUIK_PT_armature_options,
 )
 
-def register():
-    importlib.reload(dublf)
+def register_experimental_2d():
+    # Reload
+    importlib.reload(layers)
+    importlib.reload(oca)
+    # Modules
+    layers.register()
+    oca.register()
+
+def unregister_experimental_2d():
+    layers.unregister()
+    oca.unregister()
+    
+def unregister_experimental_rig():
+    # Attributes
+    del bpy.types.Armature.duik_rig_type
+    del bpy.types.Armature.duik_layers_leg_ikfk
+    del bpy.types.Armature.duik_layers_arm_ikfk
+    # Modules
+    autorig.unregister()
+    selection_sets.unregister()
+    ui_controls.unregister()
+    ui_layers.unregister()
+    tex_anim.unregister()
+    cam_linker.unregister()
+
+def register_experimental_rig():
+    # Reload
     importlib.reload(autorig)
     importlib.reload(selection_sets)
     importlib.reload(cam_linker)
     importlib.reload(ui_controls)
     importlib.reload(ui_layers)
     importlib.reload(tex_anim)
-    importlib.reload(layers)
-    importlib.reload(oca)
-
-    # register
-    for cls in classes:
-        bpy.utils.register_class(cls)
-
-    # modules
-    dublf.register()
+    # Modules
     autorig.register()
     selection_sets.register()
     cam_linker.register()
     ui_controls.register()
     ui_layers.register()
     tex_anim.register()
-    layers.register()
-    oca.register()
-
-    # add options to armature
+    # Attributes
     if not hasattr( bpy.types.Armature, 'duik_rig_type' ):
         rig_types = rig_types = [
             ('custom', "Custom", "A custom character.", '', 0),
@@ -169,26 +216,33 @@ def register():
             default = True
         )
     
+def register():
+    # reload
+    importlib.reload(dublf)
+
+    # register
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # modules
+    dublf.register()
+           
 def unregister():
+    # preferences
+    preferences = bpy.context.preferences.addons[__name__].preferences
+
+    if preferences.use_experimental_rig:
+        unregister_experimental_rig()
+
+    if preferences.use_experimental_2d:
+        unregister_experimental_2d()
+
+    # modules
+    dublf.unregister()   
+
     # unregister
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
-
-    del bpy.types.Armature.duik_rig_type
-    del bpy.types.Armature.duik_layers_leg_ikfk
-    del bpy.types.Armature.duik_layers_arm_ikfk
-
-    # modules
-    dublf.unregister()
-    autorig.unregister()
-    selection_sets.unregister()
-    ui_controls.unregister()
-    ui_layers.unregister()
-    tex_anim.unregister()
-    cam_linker.unregister()
-    layers.unregister()
-    oca.unregister()
-
 
 if __name__ == "__main__":
     register()
